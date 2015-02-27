112cf9fadb0b27643fc7aca157e15f7553a9c2a9
Amy Palamountain <amy@palamounta.in>
2015-02-27 13:46:13 -0800

    Repro

---
+++ XunitTypeBug/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Windows;
+using AnotherAssembly;
+using Xunit;
+
+namespace XunitTypeBug
+{
+    public class Tests
+    {
+        [Theory]
+        [InlineData(typeof(int))]
+        [InlineData(typeof(string))]
+        [InlineData(typeof(MyType))]
+        [InlineData(typeof(ExternalType))]
+        [InlineData(typeof(GridLength))]
+        [InlineData(typeof(SqlCommand))]
+        public void TypeOfTests(Type type)
+        {
+            Assert.NotNull(type);
+        }
+    }
+
+    public class MyType{}
+}
